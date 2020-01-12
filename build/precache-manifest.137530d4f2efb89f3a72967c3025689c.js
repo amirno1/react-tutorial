2f33c4c5591fe387dbfcb1953d9cdbb9f2ea6119
Amir <amir@Amirs-MacBook-Pro.local>
2020-01-12 14:10:58 +0100

    build and deploy

---
+++ build/precache-manifest.137530d4f2efb89f3a72967c3025689c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22b805ba8014a4cc00f4e0803e4aff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9613c6663e58332023e",
+    "url": "/static/css/main.290abac2.chunk.css"
+  },
+  {
+    "revision": "16280314f1b5de67c0e1",
+    "url": "/static/js/2.efe41a09.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.efe41a09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9613c6663e58332023e",
+    "url": "/static/js/main.61b68172.chunk.js"
+  },
+  {
+    "revision": "b393b1e110daa36721dc",
+    "url": "/static/js/runtime-main.1699a156.js"
+  }
+]);
